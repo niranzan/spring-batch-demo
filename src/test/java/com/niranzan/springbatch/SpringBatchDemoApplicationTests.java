2d5c8b51204904411691eb6638c5fed1c4484d28
Niranjan Reddy G <niranzan.g@gmail.com>
2024-07-13 15:31:48 +0530

    Sonarqube integration

--- src/test/java/com/niranzan/springbatch/SpringBatchDemoApplicationTests.java
+++ src/test/java/com/niranzan/springbatch/SpringBatchDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.niranzan.springbatch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringBatchDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.debug("contextLoads");
 	}
 
 }
